f7a1ad063124a5d2a976145727ca403f4de22f28
bishops250 <bishops250@gmail.com>
2019-09-30 15:05:38 -0400

    Just Started

---
+++ src/RectangleFun.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class RectangleFun {
+
+    public static void main(String[]args){
+
+        int rectangle_x_coordinate, rectangle_y_coordinate;
+
+        double rectangle_width, rectangle_height, rectangle_height, rectangle_width;
+
+        Scanner user_input = new Scanner(System.in);
+
+
+        System.out.println(" Please enter the X coordinate: ");
+        rectangle_x_coordinate = user_input.nextInt();
+
+        System.out.println(" Please enter the Y coordinate: ");
+        rectangle_y_coordinate = user_input.nextInt();
+
+        System.out.println("Please enter the width of the rectangle: ");
+        rectangle_width = user_input.nextInt();
+
+        System.out.println("Please enter the length of the rectangle: ");
+        rectangle_height = user_input.nextInt();
+
+        Point point_rectangle = new Point(rectangle_x_coordinate,rectangle_y_coordinate);
+
+
+
+
+
+    }
+
+

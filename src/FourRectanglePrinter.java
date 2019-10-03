22faa45b1596329d56454e306943cf1eaa261fb3
bishops250 <bishops250@gmail.com>
2019-10-03 09:37:53 -0400

    Fixed Four Rectangles

--- src/FourRectanglePrinter.java
+++ src/FourRectanglePrinter.java
@@ -6,18 +6,16 @@ public class FourRectanglePrinter {
 
     public static void main(String[]args) {
 
-        int rectangle_x_coordinate = 0, rectangle_y_coordinate = 0;
-
 
         Scanner user_input = new Scanner(System.in);
 
 
         System.out.println("Please enter the width of the rectangles: ");
-       final int rectangle_width = user_input.nextInt(); /*Set the height and width to final because all the boxes will
+        int rectangle_width = user_input.nextInt(); /*Set the height and width to final because all the boxes will
        have the same size just a different location */
 
         System.out.println("Please enter the length of the rectangles: ");
-       final int rectangle_height = user_input.nextInt();
+        int rectangle_height = user_input.nextInt();
 
         Rectangle rectangle_one= new Rectangle(rectangle_width,rectangle_height);
         //creates the first rectangle at 0,0

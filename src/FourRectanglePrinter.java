37bb14c03c7ae89e56ec4708daeeb608ee03c738
bishops250 <bishops250@gmail.com>
2019-10-03 09:36:42 -0400

    Fixed Four Rectangles

--- src/FourRectanglePrinter.java
+++ src/FourRectanglePrinter.java
@@ -19,19 +19,15 @@ public class FourRectanglePrinter {
         System.out.println("Please enter the length of the rectangles: ");
        final int rectangle_height = user_input.nextInt();
 
-        Rectangle rectangle_one= new Rectangle(rectangle_x_coordinate, rectangle_y_coordinate, rectangle_width,
-                rectangle_height); //creates the first rectangle at 0,0
+        Rectangle rectangle_one= new Rectangle(rectangle_width,rectangle_height);
+        //creates the first rectangle at 0,0
+        rectangle_one.translate(2,0);
+        System.out.println(rectangle_one);
+        rectangle_one.translate(2,3);
+        System.out.println(rectangle_one);
+        rectangle_one.translate(0,3);
+        System.out.println(rectangle_one);
 
-        Rectangle rectangle_two= new Rectangle(2,0,rectangle_width, rectangle_height);
-        //creates the second rectangle and sets it so its located to left of the first rectangle
-
-
-
-        Rectangle rectangle_three= new Rectangle(2,3,rectangle_width,rectangle_height);
-        //creates the thrid rectangle and sets it under the second rectangle
-
-        Rectangle rectangle_four= new Rectangle(0,3,rectangle_width,rectangle_height);
-       //creates fourth rectangle and moves it under the first rectangle
 
 
     }

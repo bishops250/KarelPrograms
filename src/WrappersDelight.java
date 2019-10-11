a3ba9df6e482c4bd776bb4f9e645f9423cec9884
bishops250 <biishops250@gmail.com>
2019-10-10 22:34:44 -0400

    I believed I completed Wrapper delight

--- src/WrappersDelight.java
+++ src/WrappersDelight.java
@@ -1,2 +1,24 @@
+
 public class WrappersDelight {
+
+    public static void main(String[]args) {
+
+        int num1 = 14;
+
+         Integer num2 = new Integer(8);
+
+         double num3 = 2.5;
+
+         double num4 = num3 + num2;
+
+         num2= 5;
+
+         num3 =  num1 + num2 + num4;
+
+         num1 = (int) (num2 + num4);
+
+
+
+
+    }
 }

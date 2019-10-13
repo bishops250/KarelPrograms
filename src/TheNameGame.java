46aca6eacc077a51ceb1af4c609423a88350e557
bishops250 <biishops250@gmail.com>
2019-10-12 20:43:46 -0400

    Almost there I got it down to one method just correcting the output.

--- src/TheNameGame.java
+++ src/TheNameGame.java
@@ -3,60 +3,32 @@ import java.util.Scanner;
 
 public class TheNameGame {
 
-    private static void EmptySpace() {
-        System.out.println(" ");
-    }
+    private static void Verse_1(String first_last_name) { //prints first line of the Chant
 
-    private static void Verse_1(String first_name) { //prints first line of the Chant
+        int first_last_length = first_last_name.length();
 
-        System.out.println( first_name.substring(0,1).toUpperCase() + first_name.substring(1,5) + " " +
-                first_name.substring(0,1).toUpperCase() + first_name.substring(1,5) + ", " + "bo-"
-                + first_name.substring(1,5));
-    }
+        System.out.println(first_last_name.substring(0,1).toUpperCase() + first_last_name.substring(1)
+                +" " + first_last_name.substring(0,1).toUpperCase() + first_last_name.substring(1) + "bo-B" +
+                first_last_name.substring(1,first_last_length));
 
-    private static void Verse_2(String last_name, String first_name) { //prints second line of the chant
-        System.out.println("Banana-fana " + "fo-" +(first_name.substring(0,1)+last_name.substring(1,4)));
+        System.out.println("Banana-fana for-F" + first_last_name.substring(1,first_last_length) + "\n "
+                + first_last_name.toUpperCase()+ "! " + "\n");
+    };
 
-    }
 
-    private static void Verse_3(String last_name) { //prints third line of the chant
-        System.out.println("Fee-fi-mo-M"+last_name.substring(1,4) + " \n" + last_name.toUpperCase() + "!");
-    }
-
-    private static void Verse_4(String last_name) {
-
-        System.out.println( last_name.substring(0,1).toUpperCase() + last_name.substring(1,4) + " " +
-                last_name.substring(0,1).toUpperCase() + last_name.substring(1,4) + ", " + "bo-"
-                + last_name.substring(1,4));
-
-    }
-
-    private static void Verse_5(String last_name, String first_name) {
-        System.out.println("Banana-fana " + "fo-" +(first_name.substring(0,1)+last_name.substring(1,4)));
-
-    }
-
-    private static void Verse_6(String last_name) {
-        System.out.println("Fee-fi-mo-M"+last_name.substring(1,4) + " \n" + last_name.toUpperCase() + "!");
-
-    }
     public static void main(String[]args) {
 
         Scanner user_input = new Scanner(System.in);
 
         System.out.println(" Please enter your first name: ");
-        String first_name = user_input.next();
+        String first_name = user_input.nextLine();
 
         System.out.println(" Please enter your last name: ");
-        String last_name = user_input.next() ;
+        String last_name = user_input.nextLine();
 
         Verse_1(first_name);
-        Verse_2(first_name,last_name);
-        Verse_3(first_name);
-        EmptySpace();
-        Verse_4(last_name);
-        Verse_5(last_name, first_name);
-        Verse_6(last_name);
+        Verse_1(last_name);
+
 
     }
 }

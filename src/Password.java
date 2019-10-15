1cfd3be7efa9d1d01fc5ebe0c9d7c39ab08c8ca7
bishops250 <bishops250@gmail.com>
2019-10-15 09:00:44 -0400

    working on password.

---
+++ src/Password.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Password {
+
+    public static void main(String[]args) {
+
+        Scanner user_input = new Scanner(System.in);
+
+        String first_name, middle_name, last_name, users_password;
+
+        int users_age;
+
+        System.out.println("Please enter your first name: ");
+        first_name = user_input.next();
+
+        System.out.println("Please enter your middle name: ");
+        middle_name = user_input.next();
+
+        System.out.println("Please enter your last name: ");
+        last_name = user_input.next();
+
+        System.out.println("Please enter your age: ");
+        users_age = user_input.nextInt();
+
+        int first_name_string_length = (first_name.length()/2);
+        int middle_name_string_length = (middle_name.length()/2);
+        int last_name_string_length = (last_name.length()/2);
+        String first_name_string_length =;
+
+        users_password =
+
+
+
+
+
+
+    }
+
+}

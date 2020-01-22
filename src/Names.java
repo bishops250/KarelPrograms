299dc809c09d18041ea00bb98261c7ae13d17d89
bishops250 <biishops250@gmail.com>
2020-01-22 18:14:37 -0500

    Added the Statement to print the last and first index of the individuals name.

--- src/Names.java
+++ src/Names.java
@@ -21,24 +21,22 @@ public class Names {
 
         int first_lastname_length = first_last_name.length(); //outputs the length of names combined
 
-        String users_initials =user_first_name.substring(0,1) +user_last_name.substring(0,1); /* gets first letter of
+        String users_initials = user_first_name.substring(0, 1) + user_last_name.substring(0, 1); /* gets first letter of
         users  first name and last name and creates initials.*/
 
         int index_firstname_last_letter = user_first_name.indexOf(user_last_name);
 
         int index_lastname_last_latter = user_last_name.indexOf(user_first_name);
 
-        String mid_first =((user_first_name.substring(0, user_first_name.length() / 2)) +
-                (user_last_name.substring(user_last_name.length() / 2, user_last_name.length())));;
+        String mid_first = ((user_first_name.substring(0, user_first_name.length() / 2)) +
+                (user_last_name.substring(user_last_name.length() / 2)));
 
-        String mid_last =((user_last_name.substring(0, user_last_name.length() / 2)) +
-                (user_first_name.substring(user_first_name.length() / 2, user_first_name.length()))) ;
+        String mid_last = ((user_last_name.substring(0, user_last_name.length() / 2)) +
+                (user_first_name.substring(user_first_name.length() / 2)));
 
-        int compare_first_last = user_first_name.compareTo(user_last_name) ; //outputs -1
+        int compare_first_last = user_first_name.compareTo(user_last_name); //outputs -1
 
         int compare_last_first = user_last_name.compareTo(user_first_name); //outputs -1
-
-
 
 
         System.out.println(user_first_name);
@@ -47,8 +45,10 @@ public class Names {
         System.out.println(users_initials);
         System.out.println(first_lastname_length);
         System.out.println(compare_first_last);
-        System.out.println(compare_last_first *-1);
+        System.out.println(compare_last_first * -1);
         System.out.println(mid_first + " " + mid_last);
+        System.out.println(index_firstname_last_letter);
+        System.out.println(index_lastname_last_latter);
 
 
     }
